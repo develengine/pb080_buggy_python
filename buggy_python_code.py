4008d3f4baa0418ac744cac0262f9c3dea81d0df
unknown <xboros1@ad.fi.muni.cz>
2022-04-27 14:51:39 +0200

    Did some stuff lol.

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,33 +1,38 @@
 # contains bunch of buggy examples
 # taken from https://hackernoon.com/10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
+import subprocess
 import cPickle
-import subprocess
 import base64
-import subprocess
 import flask
 
 # Input injection
 def transcode_file(request, filename):
+    '''An epic docstring.'''
     command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=filename)
     subprocess.call(command, shell=True)  # a bad idea!
 
 
 # Assert statements
-def foo(request, user):
+def bruh(request, user):
+    '''An epic docstring.'''
     assert user.is_admin, 'user does not have access'
     # secure code...
 
 
 # Pickles
-class RunBinSh(object):
+class RunBinSh:
+    '''An epic docstring.'''
     def __reduce__(self):
+        '''An epic docstring.'''
         return (subprocess.Popen, (('/bin/sh',),))
 
 def import_urlib_version(version):
+    '''An epic docstring.'''
     exec("import urllib%s as urllib" % version)
 
 @app.route('/')
 def index():
+    '''An epic docstring.'''
     module = flask.request.args.get("module")
     import_urlib_version(module)
 
